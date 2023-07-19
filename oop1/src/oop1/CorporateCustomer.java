470926fe1c6ecfb37116bfac8774bcad8802fefe
Yunus Emre GÜRLER <127211042+YunusEmreGU@users.noreply.github.com>
2023-07-19 23:20:39 +0300

    oop1
    oop1

---
+++ oop1/src/oop1/CorporateCustomer.java
@@ -0,0 +1,22 @@
+package oop1;
+
+public class CorporateCustomer extends Customer {
+	private String companyName;
+	private String taxtNumber;
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getTaxtNumber() {
+		return taxtNumber;
+	}
+
+	public void setTaxtNumber(String taxtNumber) {
+		this.taxtNumber = taxtNumber;
+	}
+}
